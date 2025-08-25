bd98a1bff5753f456abba9eeda4f7cc9c4fb911b
Antoni Seba <antoniwoj@o2.pl>
2025-08-25 13:14:27 +0800

    okej

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const { smartSyncScheduler } = require('./smart_sync_scheduler');
 const { ordersSyncScheduler } = require('./orders_sync_scheduler');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors());
